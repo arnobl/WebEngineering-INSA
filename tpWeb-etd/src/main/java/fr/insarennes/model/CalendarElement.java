f53d6d25d8c5befbc59234c053fc5e98869cdb82
arnobl <blouin.arno@gmail.com>
2018-04-26 12:10:27 +0200

    fix swagger primary key

--- tpWeb-etd/src/main/java/fr/insarennes/model/CalendarElement.java
+++ tpWeb-etd/src/main/java/fr/insarennes/model/CalendarElement.java
@@ -1,6 +1,9 @@
 package fr.insarennes.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public abstract class CalendarElement {
+	@ApiModelProperty(hidden = true) // This attribute (primary key) will be ignored by Swagger
 	protected int id;
 
 	public int getId() {
