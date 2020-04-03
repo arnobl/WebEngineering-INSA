90c99f354ab8d24e90e76292810c547eb8a9ad42
arnobl <1052622+arnobl@users.noreply.github.com>
2020-04-03 14:43:12 +0200

    jersey example update

--- rest/jersey/example/src/main/java/fr/insa/rennes/web/Main.java
+++ rest/jersey/example/src/main/java/fr/insa/rennes/web/Main.java
@@ -1,26 +1,32 @@
 package fr.insa.rennes.web;
 
+import fr.insa.rennes.web.resource.AlbumResource;
 import fr.insa.rennes.web.utils.MyExceptionMapper;
 import java.io.IOException;
 import java.net.URI;
 import javax.ws.rs.client.ClientBuilder;
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
-public class Main {
+public final class Main {
 	// Base URI the Grizzly HTTP server will listen on
 	public static final String HTTP_ADDRESS = "http://localhost:4444/";
+
+	private Main() {
+		super();
+	}
 
 	/**
 	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
 	 * @return Grizzly HTTP server.
 	 */
 	public static HttpServer startServer() {
-		final ResourceConfig rc = new ResourceConfig()
-			.packages("fr.insa.rennes.web.resource")
+		final ResourceConfig rc = new ResourceConfig(AlbumResource.class)
 			.register(MyExceptionMapper.class)
+			.register(MoxyJsonFeature.class)
 			.register(io.swagger.jaxrs.listing.ApiListingResource.class)
 			.register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
 
@@ -32,7 +38,11 @@ public class Main {
 	public static void main(final String[] args) throws IOException {
 		final HttpServer server = startServer();
 		// Required to access the web pages stored in the webapp folder.
-		server.getServerConfiguration().addHttpHandler(new StaticHttpHandler("src/main/webapp"), "/myFirstWebApp/");
+		final ClassLoader loader = Main.class.getClassLoader();
+		final CLStaticHttpHandler docsHandler = new CLStaticHttpHandler(loader, "swag/");
+		docsHandler.setFileCacheEnabled(false);
+
+		server.getServerConfiguration().addHttpHandler(docsHandler, "/swag/");
 		ClientBuilder.newClient().target(HTTP_ADDRESS);
 		System.in.read();
 		server.shutdownNow();
