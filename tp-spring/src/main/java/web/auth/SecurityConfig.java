58c25f1b210560caf9203900968cd211b3cca111
arnobl <1052622+arnobl@users.noreply.github.com>
2022-04-13 18:13:16 +0200

    up

---
+++ tp-spring/src/main/java/web/auth/SecurityConfig.java
@@ -0,0 +1,20 @@
+package web.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/api/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.csrf().disable();
+	}
+}
