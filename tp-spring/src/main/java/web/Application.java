2bfede583b86971d9a65dfefe4fe6250caf829b6
arnobl <1052622+arnobl@users.noreply.github.com>
2022-04-29 11:41:53 +0200

    up

--- tp-spring/src/main/java/web/Application.java
+++ tp-spring/src/main/java/web/Application.java
@@ -2,10 +2,23 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class Application {
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	// Just for seeing requests in the console
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		final CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(false);
+		return loggingFilter;
 	}
 }
