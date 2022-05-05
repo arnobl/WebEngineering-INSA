2825026a3b763235ba69bcdac2ad9dc93a1e702b
arnobl <1052622+arnobl@users.noreply.github.com>
2022-05-05 08:40:37 +0200

    up

---
+++ tp-spring/src/test/java/web/controller/TestTodoV1.java
@@ -0,0 +1,9 @@
+package web.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestTodoV1 {
+}
