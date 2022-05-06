e5c1577601c883acd5302abd09920ee77b412743
arnobl <1052622+arnobl@users.noreply.github.com>
2022-05-06 12:04:13 +0200

    up

--- tp-spring/src/test/java/web/controller/TestTodoV1.java
+++ tp-spring/src/test/java/web/controller/TestTodoV1.java
@@ -1,9 +1,15 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class TestTodoV1 {
+	@Autowired
+	private MockMvc mvc;
 }
