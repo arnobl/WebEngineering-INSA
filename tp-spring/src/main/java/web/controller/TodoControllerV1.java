b98d49c9b994e6cf728c6c747d7ff812f97352d9
arnobl <1052622+arnobl@users.noreply.github.com>
2023-02-19 11:02:08 +0100

    update

---
+++ tp-spring/src/main/java/web/controller/TodoControllerV1.java
@@ -0,0 +1,29 @@
+package web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import web.model.Category;
+import web.model.Todo;
+
+@RestController
+@RequestMapping("api/v1/todo")
+public class TodoControllerV1 {
+	public TodoControllerV1() {
+	}
+
+	@GetMapping(path = "hello", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String hello() {
+		return "Hello";
+	}
+}
