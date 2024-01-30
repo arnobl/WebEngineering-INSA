1e26bb50972b0c4533c30eff2af46be9cacad863
arnobl <1052622+arnobl@users.noreply.github.com>
2024-01-30 14:45:21 +0100

    update

--- tp-spring/src/main/java/tpspring/controller/HelloController.java
+++ tp-spring/src/main/java/tpspring/controller/HelloController.java
@@ -1,8 +1,15 @@
 package tpspring.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +20,8 @@ record Message(String txt) {}
 @RequestMapping("api/v1/public/hello")
 @CrossOrigin
 public class HelloController {
+	private final List<String> txts = new ArrayList<>();
+
 	@GetMapping(path = "helloworld", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String hello() {
 		return "Hello World";
@@ -23,4 +32,14 @@ public class HelloController {
 		// Marshalling
 		return new Message("Hello world!");
 	}
+
+    @PostMapping(path = "txt", consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void newTxt(@RequestBody final String txt) {
+        txts.add(txt);
+    }
+
+    @GetMapping(path = "txt", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getTxts() {
+        return txts;
+    }
 }
