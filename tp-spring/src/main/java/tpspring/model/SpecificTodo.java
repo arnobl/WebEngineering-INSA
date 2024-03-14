db12fad1fe30c0dec855bad1f91256891560e06d
arnobl <1052622+arnobl@users.noreply.github.com>
2024-03-14 09:56:52 +0100

    fix

--- tp-spring/src/main/java/tpspring/model/SpecificTodo.java
+++ tp-spring/src/main/java/tpspring/model/SpecificTodo.java
@@ -1,5 +1,18 @@
 package tpspring.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
 public class SpecificTodo extends Todo {
 	private String specificAttr;
 }
