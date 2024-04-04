c77cfce2ed8e803d2f833f78b854c05c5d6430ca
arnobl <1052622+arnobl@users.noreply.github.com>
2024-04-04 09:22:53 +0200

    fix

--- rest/springboot2/src/main/java/fr/insarennes/demo/service/UserService.java
+++ rest/springboot2/src/main/java/fr/insarennes/demo/service/UserService.java
@@ -1,13 +1,7 @@
-package fr.insarennes.demo.service;
+package web.service;
 
-import fr.insarennes.demo.model.User2;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import lombok.Getter;
+import java.util.List;
+
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,27 +9,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class UserService {
 	private final PasswordEncoder passwordEncoder;
 
 	private final UserDetailsManager userDetailsManager;
 
 	private final HttpServletRequest request;
-
-	@Getter
-	private final Map<String, User2> users;
-
-	public UserService(final PasswordEncoder passwordEncoder,
-		final UserDetailsManager userDetailsManager,
-		final HttpServletRequest request) {
-		super();
-		this.passwordEncoder = passwordEncoder;
-		this.userDetailsManager = userDetailsManager;
-		this.request = request;
-		users = new HashMap<>();
-	}
-
 
 	/**
 	 * @throws IllegalArgumentException If already exists
@@ -45,13 +31,12 @@ public class UserService {
 			throw new IllegalArgumentException("Not possible");
 		}
 
-		final UserDetails user = new User(login, passwordEncoder.encode(pwd),
-			Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+		final UserDetails user = new User(login, passwordEncoder.encode(pwd), List.of(new SimpleGrantedAuthority("ROLE_USER")));
 		userDetailsManager.createUser(user);
-		users.put(login, new User2(login, "", ""));
 	}
 
 	public void delAccount(final String name) {
+		// warning: IRL, should remove associated data as well
 		userDetailsManager.deleteUser(name);
 	}
 
@@ -77,7 +62,8 @@ public class UserService {
 		return true;
 	}
 
-	public User2 findUser(final String userName) {
-		return users.get(userName);
+	public UserDetails findUser(final String userName) {
+		return userDetailsManager.loadUserByUsername(userName);
 	}
 }
+
